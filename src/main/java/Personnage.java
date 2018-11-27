797b9354b9c21ccebda45f4cc86a06195d2ce899
Babassi <15236@ecam.be>
2018-11-27 13:52:40 +0100

    Test checkstyle

--- src/main/java/Personnage.java
+++ src/main/java/Personnage.java
@@ -2,65 +2,59 @@ import java.util.Random;
 
 public abstract class Personnage {
 
-    protected int pv;
-    protected int force;
-    protected int energie = 100;
-    protected int defense = 0;
-    protected int minDamage = 5;
-    protected int maxDamage = 10;
-    protected boolean mort;
+  protected int pv;
+  protected int force;
+  protected int energie = 100;
+  protected int defense = 0;
+  protected int minDamage = 5;
+  protected int maxDamage = 10;
+  protected boolean mort;
 
-    public boolean getMort(){
-        return mort;
+  public boolean getMort() {
+    return mort;
+  }
+
+  public void setMort() {
+    this.mort = true;
+  }
+
+  public int attaquer() {
+    this.energie = -40;
+    Random rand = new Random();
+    int degats = (rand.nextInt(maxDamage - minDamage + 1) + minDamage); // * force/10;
+    return degats;
+  }
+
+  public void subir(int degats) {
+    int newPV = (this.getPV() - (degats * (100 - defense) / 100));
+
+    this.setPV(newPV);
+
+    if (newPV <= 0) {
+      this.setPV(0);
+      this.setMort();
     }
-    public void setMort(){
-        this.mort = true;
-    }
+    this.defense = 0;
+  }
 
-    public int attaquer()
-    {
-        this.energie =- 40;
-        Random rand = new Random();
-        int degats = (rand.nextInt(maxDamage - minDamage + 1) + minDamage);// * force/10;
-        return degats;
-    }
+  public void gainEnergy() {
+    this.energie = +15;
+  }
 
-    public void subir(int degats)
-    {
-        int newPV = (this.getPV()- (degats * (100 - defense)/100 ));
+  public int showEnergy() {
+    return this.energie;
+  }
 
-        this.setPV(newPV);
+  public int getPV() {
+    return this.pv;
+  }
 
-        if (newPV <= 0)
-        {
-            this.setPV(0);
-            this.setMort();
-        }
-        this.defense = 0;
-    }
+  protected void setPV(int pv) {}
 
-    public void gainEnergy()
-    {
-        this.energie =+ 15;
-    }
+  public void parer() {
+    this.energie = -15;
+    this.defense = 70;
+  };
 
-    public int showEnergy()
-    {
-        return this.energie;
-    }
-
-
-    public int getPV(){
-        return this.pv;
-    }
-
-    protected void setPV(int pv) {
-    }
-    public void parer(){
-        this.energie =- 15;
-        this.defense = 70;
-    };
-
-    public abstract void draw();
-
+  public abstract void draw();
 }
