f9c85838c78d63f0c488f08dff15fb4266a55e3f
adrienpetit <32298348+adrienpetit@users.noreply.github.com>
2018-11-27 22:13:26 +0100

    Add files via upload

--- src/main/java/Geometre.java
+++ src/main/java/Geometre.java
@@ -1,5 +1,14 @@
+/** Represente un geometre
+ * @author adrien
+ */
 public class Geometre extends Personnage {
 
+  /**
+   * Initialise geometre
+   * @param pv
+   * @param force
+   * @param energie
+   */
   public Geometre(int pv, int force, int energie) {
     this.pv = pv;
     this.force = force;
@@ -7,4 +16,12 @@ public class Geometre extends Personnage {
     this.minDamage = -1;
     this.maxDamage = +2;
   }
+
+  /**
+   * Affiche Les infos du géomètre
+   */
+  public void draw() {
+    System.out.println("Geometre: " + pv + ", " + force + "," + energie);
+  }
+
 }
--- src/main/java/Informaticien.java
+++ src/main/java/Informaticien.java
@@ -1,9 +1,26 @@
+/** Represente un informaticien
+ * @author adrien
+ */
 public class Informaticien extends Personnage {
 
+
+  /**
+   * Initialise l'informaticien
+   * @param pv
+   * @param force
+   */
   public Informaticien(int pv, int force) {
     this.pv = pv;
     this.force = force;
     this.maxDamage = 50;
+  }
+
+  /**
+   * Affiche Les infos de l'informaticien
+   */
+  public void draw() {
+    System.out.println(
+        "Informaticien: " + "Point de vie :" + pv + " Force : " + force + " Energie :" + energie);
   }
 
 }
