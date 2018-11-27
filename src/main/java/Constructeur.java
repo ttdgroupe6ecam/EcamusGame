c58ce48e5ef29c43dc5c4f63966b344b48995777
adrienpetit <32298348+adrienpetit@users.noreply.github.com>
2018-11-27 22:36:39 +0100

    Add files via upload

--- src/main/java/Constructeur.java
+++ src/main/java/Constructeur.java
@@ -1,11 +1,24 @@
 class Constructeur extends Personnage {
 
+  /**
+   * Constructeur.
+   * @param pv
+   * @param force
+   * @param energie 
+   */
   public Constructeur(int pv, int force, int energie) {
     this.pv = pv;
     this.force = force;
     this.energie = energie;
     this.minDamage = -3;
     this.maxDamage = +4;
+  }
+
+  /**
+   * Affiche Les infos du constructeurs.
+   */
+  public void draw() {
+    System.out.println("Constructeur: " + pv + ", " + force + "," + energie);
   }
 
 }
