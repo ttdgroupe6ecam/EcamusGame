aabb775cfc65fbc7f8c94c7444e1f0920388b56a
amlen <yass.d@outlook.com>
2018-12-05 11:51:15 +0100

    add unitaire tests

---
+++ src/test/java/ElectronicienTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MediaMonster
+ */
+public class ElectronicienTest {
+    
+    public ElectronicienTest() {
+        Electronicien electronicien = new Electronicien(100, 50);
+        assertNotNull(electronicien);
+    }
+    
+ 
+}
---
+++ src/test/java/GeometreTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MediaMonster
+ */
+public class GeometreTest {
+    
+    public GeometreTest() {
+        Geometre informaticien = new Geometre(100, 50,200);
+        assertNotNull(informaticien);  
+    }
+   
+}
