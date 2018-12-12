8d8fe735cc03c97ec69691ffc8422fac11e9723e
amlen <yass.d@outlook.com>
2018-12-12 11:31:05 +0100

    upsdate Mecanicien constract

--- src/main/java/Mecanicien.java
+++ src/main/java/Mecanicien.java
@@ -1,8 +1,7 @@
 public class Mecanicien extends Personnage {
 
-  public Mecanicien(final int pv,final int force,final int energie) {
-    this.pv = pv;
-    this.force = force;
+  public Mecanicien(final int pv,final int force,final int energie,final boolean mort) {
+    super(pv,  force,mort);
     this.energie = energie;
     this.minDamage = +2;
     this.maxDamage = +2;
