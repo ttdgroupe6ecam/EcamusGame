57e2b4ce221d7760f5ebbeaef26076880b42a9de
amlen <yass.d@outlook.com>
2018-12-17 00:08:56 +0100

    Update Automaticien.java
    add java doc

--- src/main/java/Automaticien.java
+++ src/main/java/Automaticien.java
@@ -1,5 +1,13 @@
+/**
+ * Automaticien class.
+ * @author MediaMonster
+ */
 public class Automaticien extends Personnage {
-
+ /**
+     * @param pv **point de vie **
+     * @param force ** la force  **
+     * @param energie **l'énergie **
+     */
   public Automaticien(final int pv,final int force,final int energie) {
     this.pv = pv;
     this.force = force;
