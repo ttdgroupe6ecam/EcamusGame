2a4449ac712d132c8491a63fccdb942520721985
amlen <yass.d@outlook.com>
2018-12-17 00:15:39 +0100

    Update Electronicien.java
    add java doc

--- src/main/java/Electronicien.java
+++ src/main/java/Electronicien.java
@@ -7,7 +7,7 @@ class Electronicien extends Personnage {
      * @param pv **point de vie **
      * @param force ** la force  **
      */
-  public Electronicien(final int pv,final int force) {
+  public Electronicien(final int pv, final int force) {
     this.pv = pv;
     this.force = force;
     // this.minDamage = 0;
