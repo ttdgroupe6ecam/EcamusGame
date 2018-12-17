d4ad1f6b519cbf5a7277f7b8f77acf6e355ca690
Xcrid <36483768+Xcrid@users.noreply.github.com>
2018-12-17 15:56:42 +0100

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/**
+ * main class.
+ * @author MediaMonster
+ */
+import javax.swing.*;
+/**
+*main function.
+*/
+public class Main {
+  /**
+  *constructor.
+  *@param args the argument of the method
+  */
+  public static void main(String[] args) {
+
+    Screen s = new Screen();
+    JFrame window;
+      //window = s.window;
+  }
+}
