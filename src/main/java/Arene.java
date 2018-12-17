5d20582301af16ffb7430b49d3433ef917799c0b
Xcrid <venonarthur.venon4@gmail.com>
2018-12-17 13:52:13 +0100

    checkstyle sun

--- src/main/java/Arene.java
+++ src/main/java/Arene.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+/**
+ * this is thc class that contains the setup method for the game and,
+ * the one to play the game.
+ * @author venon arthur
+ */
 public class Arene {
-
-  public static List<Decorator> listPersonnages = new ArrayList<Decorator>();
-
-
-  
- 
   /**
-   * Declaration de l'interface en invite de commande
-   * Permet de choisir son personnage et son arme
+   * A static list that contains all the characters played by the users.
    */
-  public static void BuildFight() {
+  private static List<Decorator> listPersonnages = new ArrayList<Decorator>();
+  /**
+   * Declaration de l'interface en invite de commande,
+   * Permet de choisir son personnage et son arme.
+   */
+  public static void buildFight() {
 
     System.out.println("Bienvenue sur Ecamus");
     int x = 0;
@@ -25,48 +27,46 @@ public class Arene {
 
         System.out.println("Choisissez un adversaire :");
       }
-      System.out.println("Tapez : 1 -> informaticien" + " " +
-              " 2 -> Electronicien, 9->quitter ");
+      System.out.println("Tapez : 1 -> informaticien " 
+              +" 2 -> Electronicien, 9->quitter ");
 
       Scanner sc = new Scanner(System.in);
 
       //str représente le choix du personnage
       int str = Integer.parseInt(sc.nextLine());
       if (str == 1) {
-        System.out.println("Tapez: 1 -> souris" + " " + 
-                " 2 -> clé à molette");
+        System.out.println("Tapez: 1 -> souris " + " 2 -> clé à molette");
         Scanner sc2 = new Scanner(System.in);
 
         //str2 représente le choix de l'arme
         int str2 = Integer.parseInt(sc2.nextLine());
         if (str2 == 1) {
-          Decorator personnage = 
-                  new sourisDecorator(new Informaticien(100, 24));
+          Decorator personnage;
+            personnage = new sourisDecorator(new Informaticien(100, 24));
           listPersonnages.add(personnage);
           System.out.println(listPersonnages);
         }
         if (str2 == 2) {
-          Decorator personnage = 
-                  new cleMoletteDecorator(new Informaticien(100, 24));
+          Decorator personnage;
+            personnage = new cleMoletteDecorator(new Informaticien(100, 24));
           listPersonnages.add(personnage);
           System.out.println(listPersonnages);
         }
       }
 
       if (str == 2) {
-        System.out.println("Tapez: 1 -> souris" + " " 
-                + " 2 -> clé à molette");
+        System.out.println("Tapez: 1 -> souris " + " 2 -> clé à molette");
         Scanner sc2 = new Scanner(System.in);
         int str2 = Integer.parseInt(sc2.nextLine());
         if (str2 == 1) {
-          Decorator personnage = 
-                  new sourisDecorator(new Electronicien(100, 24));
+          Decorator personnage;
+            personnage = new sourisDecorator(new Electronicien(100, 24));
           listPersonnages.add(personnage);
           System.out.println(listPersonnages);
         }
         if (str2 == 2) {
-          Decorator personnage = 
-                  new cleMoletteDecorator(new Electronicien(100, 24));
+          Decorator personnage;
+            personnage = new cleMoletteDecorator(new Electronicien(100, 24));
           listPersonnages.add(personnage);
           System.out.println(listPersonnages);
         }
@@ -79,14 +79,15 @@ public class Arene {
 
       x = x + 1;
     }
-    Arene.startFight(
-        Arene.listPersonnages.get(0), Arene.listPersonnages.get(1)); 
+    Arene.startFight(Arene.listPersonnages.get(0),Arene.listPersonnages.get(1)); 
 // on lance le combat
   }
 
     /**
-     * Commence le combat entre les 2 personnages
-     * Interface en invite de commande, permet de choisir les actions
+     * Commence le combat entre les 2 personnages,
+     * Interface en invite de commande, permet de choisir les actions.
+     * @param personnage1 the fisrt personnage to be played
+     * @param personnage2 the second personnage to be played
      */
   public static void startFight(final Decorator personnage1,
           final Decorator personnage2) {
