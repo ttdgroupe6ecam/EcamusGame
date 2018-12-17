d214727b06aa45fbfe08608a23d091b345c384a5
amlen <yass.d@outlook.com>
2018-12-17 23:48:57 +0100

    add unit test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MediaMonster
+ */
+public class MainTest {
+    
+    @Test
+    public void TestMain() {
+      //  assertNull(new Main());
+    }
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
