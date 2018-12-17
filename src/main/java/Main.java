86bbab59cec249b9e05b23903b5af276210b4785
Xcrid <venonarthur.venon4@gmail.com>
2018-12-17 15:00:52 +0100

    checkstyle sun

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+/**
+ * th main class.
+ * @author venon arthur
+ */
+public class Main {
+  /**
+   * instanctication of main.
+   * @param args the parmaeter for the mainmethod
+   */
+  public static void main(final String[] args) {
+
+    Screen s = new Screen();
+    JFrame window;
+      //window = s.window;
+  }
+}
