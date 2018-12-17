9f29cae7c5030c6c4b774666e6ddc64e68edf9af
amlen <yass.d@outlook.com>
2018-12-17 21:01:48 +0100

    add unit test

---
+++ src/test/java/AutomaticienTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MediaMonster
+ */
+public class AutomaticienTest {
+    
+    public AutomaticienTest() {
+        Automaticien automaticien = new Automaticien(100, 50, 120);
+        assertNotNull(automaticien);
+    }
+    
+ 
+}
