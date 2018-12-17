220c68ccc233f72f8d5ccd7c8debdb65ea50c31d
amlen <yass.d@outlook.com>
2018-12-17 23:57:19 +0100

    add unit test

--- src/test/java/ScreenTest.java
+++ src/test/java/ScreenTest.java
@@ -10,28 +10,7 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 import org.junit.Assert;
 
 public class ScreenTest {
-     FrameFixture window;
-
-    @Before
-    public void setUp() {
-        Screen frame = GuiActionRunner.execute(() -> new Screen());
-        window = new FrameFixture(String.valueOf(frame));
-        window.show(); // shows the frame to t
-
-    }
-    /*
-    @Test
-    public void shouldCopyTextInLabelWhenClickingButton() {
-        comm : window.textBox("textToCopy").enterText("Some random text");
-        window.button("copyButton").click();
-        window.label("copiedText").requireText("Some random text"); comm
-        assertThat(window).hasNoNullFieldsOrProperties();
-        window.showPopupMenu();
-    }
-    @After
-    public void tearDown() {
-        window.cleanUp();
-    }*/
+  
 
     @Test
     public void TestScreen(){
