986867652092220b010fad5eb571eadb7ef60882
amlen <yass.d@outlook.com>
2018-12-17 21:40:18 +0100

    add unit test

---
+++ src/test/java/ConstructeurTest1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author MediaMonster
+ */
+public class ConstructeurTest1 {
+    
+    @Test
+    public void TestConstructeur() {
+        Constructeur constructeur = new Constructeur(10, 20, 1000);
+        assertNotNull(constructeur);
+    }
+    
+ 
+}
