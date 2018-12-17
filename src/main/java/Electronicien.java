a89ae38c5d9e704eed4bc5999096bd203b4f7c81
Xcrid <venonarthur.venon4@gmail.com>
2018-12-17 14:45:22 +0100

    checkstyle sun

--- src/main/java/Electronicien.java
+++ src/main/java/Electronicien.java
@@ -1,6 +1,14 @@
+/**
+ * this is th class to upgrade a personnage into an electronicien.
+ * @author venon arthur
+ */
 class Electronicien extends Personnage {
-
-  public Electronicien(final int pv,final int force) {
+  /**
+   * constructor of the class.
+   * @param pv the pv of the perso
+   * @param force the force of the character
+   */
+  Electronicien(final int pv,final int force) {
     this.pv = pv;
     this.force = force;
     // this.minDamage = 0;
