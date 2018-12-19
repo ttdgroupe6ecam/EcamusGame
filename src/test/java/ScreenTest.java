5b06f487567095b3c75886d33372b737ba037829
amlen <yass.d@outlook.com>
2018-12-19 09:02:26 +0100

    add get path test

--- src/test/java/ScreenTest.java
+++ src/test/java/ScreenTest.java
@@ -9,12 +9,16 @@ import org.junit.Test;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import org.junit.Assert;
 
+import java.nio.file.Paths;
+
 public class ScreenTest {
   
-/*
+
     @Test
-    public void TestScreen(){
+    public void TestGetPAth(){
         Screen s=new Screen();
-        Assert.assertEquals(s,new Screen());
-    }*/
+        String path = Paths.get(".").toAbsolutePath().normalize().toString();
+        s.setPath(path);
+        Assert.assertEquals(path,s.getPath());
+    }
 }
