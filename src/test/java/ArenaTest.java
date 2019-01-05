2ed11cc9d90348a922b697b33eb4a2df68b19d0f
Xcrid <36483768+Xcrid@users.noreply.github.com>
2019-01-05 04:30:11 +0100

    Update ArenaTest.java

--- src/test/java/ArenaTest.java
+++ src/test/java/ArenaTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.awt.GraphicsEnvironment;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
 public class ArenaTest {
 
 
