1329117812c336f19f993a2176e1d469961cf23d
Xcrid <36483768+Xcrid@users.noreply.github.com>
2019-01-05 06:52:02 +0100

    Update AreneTest.java

--- src/test/java/AreneTest.java
+++ src/test/java/AreneTest.java
@@ -10,14 +10,14 @@ public Arene arene=new Arene();
 
 @Test
     public void generateCharacter() throws Exception {
-
+        System.out.println("dfsfsdfdsfdsfsdfdsfsdfsd");
         AskString asker = mock(AskString.class);
         when(asker.askInt("1 -> informaticien 2 -> Electronicien, 9->quit")).thenReturn(1,1);
         when(asker.askInt("Tapez: 1 -> souris " + " 2 -> clé à molette")).thenReturn(2,1);
         when(asker.askInt("Joueur 1 : 1 -> attaquer , 2-> parer ")).thenReturn(1);
         when(asker.askInt("Joueur 2 : 1 -> attaquer , 2-> parer ")).thenReturn(1);
         when(asker.askInt("1: rejouer , sinon arreter le programme")).thenReturn(3);
-        System.out.println("dfsfsdfdsfdsfsdfdsfsdfsd");
+        
         arene.buildFight(asker);
         assertTrue(asker.askPrint().contains("Le Vainqueur est : Joueur"));
     }
