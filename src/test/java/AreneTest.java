4244b94f19f1ec94641e9b08537c787185a7a015
Xcrid <36483768+Xcrid@users.noreply.github.com>
2019-01-05 06:41:09 +0100

    Update AreneTest.java

--- src/test/java/AreneTest.java
+++ src/test/java/AreneTest.java
@@ -7,13 +7,13 @@ import java.io.*;
 public class AreneTest {
 
 public Arene arene=new Arene();
-@Ignore
+
 @Test
     public void generateCharacter() throws Exception {
 
         AskString asker = mock(AskString.class);
-        when(asker.askInt("1 -> informaticien 2 -> Electronicien, 9->quit")).thenReturn(1);
-        when(asker.askInt("Tapez: 1 -> souris " + " 2 -> clé à molette")).thenReturn(2);
+        when(asker.askInt("1 -> informaticien 2 -> Electronicien, 9->quit")).thenReturn(1,1);
+        when(asker.askInt("Tapez: 1 -> souris " + " 2 -> clé à molette")).thenReturn(2,1);
         when(asker.askInt("Joueur 1 : 1 -> attaquer , 2-> parer ")).thenReturn(1);
         when(asker.askInt("Joueur 2 : 1 -> attaquer , 2-> parer ")).thenReturn(1);
         when(asker.askInt("1: rejouer , sinon arreter le programme")).thenReturn(3);
