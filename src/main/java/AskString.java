42ec009ae287d71eeca1fbe532f70e46811dc0ea
Xcrid <36483768+Xcrid@users.noreply.github.com>
2019-01-05 03:51:59 +0100

    Update AskString.java
    checkstyle

--- src/main/java/AskString.java
+++ src/main/java/AskString.java
@@ -7,22 +7,27 @@ import java.util.Scanner;
 /**
  * this class replace the scanner string input and out.println,
  * by giving a simulated input for a specific output,
- * therefore we can simulate user inpiut in junit testing
+ * therefore we can simulate user inpiut in junit testing.
  * @author venon arthur
  */
 public class AskString {
-
+  /**
+   * the scanner to read user input.
+   */
   private final Scanner scanner;
+  /**
+   * the variable to know what is printed on the console.
+   */
   private final PrintStream out;
 
   /**
    *  this is the constructor to replace scanner and system.out.println.
    * @param in input (either simulated or from the console)
-   * @param out the printed string to display before getting the input
+   * @param outParam the printed string to display before getting the input
    */
-  public AskString(InputStream in, PrintStream out) {
+  public AskString(final InputStream in, final PrintStream outParam) {
     scanner = new Scanner(in);
-    this.out = out;
+    this.out = outParam;
   }
 
   /**
@@ -30,7 +35,7 @@ public class AskString {
    * @param message to be displayed
    * @return the input
    */
-  public String ask(String message) {
+  public final String ask(final String message) {
     out.println(message);
     return scanner.nextLine();
   }
@@ -39,7 +44,7 @@ public class AskString {
    * @param message to be displayed
    * @return the input
    */
-  public boolean askBool(String message) {
+  public final boolean askBool(final String message) {
     out.println(message);
     return scanner.nextBoolean();
   }
@@ -48,7 +53,7 @@ public class AskString {
    * @param message to be displayed
    * @return the input
    */
-  public double askDouble(String message) {
+  public final double askDouble(final String message) {
     out.println(message);
     return scanner.nextDouble();
   }
@@ -57,7 +62,7 @@ public class AskString {
    * @param message to be displayed
    * @return the input
    */
-  public int askInt(String message) {
+  public final int askInt(final String message) {
     out.println(message);
     return scanner.nextInt();
   }
