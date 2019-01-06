93e7fe888e4b6dacb896f319ac8cefd136f21a91
Xcrid <venonarthur.venon4@gmail.com>
2019-01-06 19:48:18 +0100

    checkstyle

--- src/test/java/AreneTest.java
+++ src/test/java/AreneTest.java
@@ -10,14 +10,14 @@ public Arene arene=new Arene();
 @Test
 @Ignore
     public void generateCharacter() throws Exception {
-        
+
         AskString asker = mock(AskString.class);
-        when(asker.ask("1 -> informaticien 2 -> Electronicien, 9->quit")).thenReturn("i","i");
-        when(asker.ask("Tapez: 1 -> souris " + " 2 -> clé à molette")).thenReturn("c","s");
-        when(asker.askInt("Joueur 1 : 1 -> attaquer , 2-> parer ")).thenReturn(1);
-        when(asker.askInt("Joueur 2 : 1 -> attaquer , 2-> parer ")).thenReturn(1);
+        when(asker.askInt("1 -> informaticien 2 -> Electronicien, 9->quit")).thenReturn(1,2);
+        when(asker.askInt("Tapez: 1 -> souris " + " 2 -> clé à molette")).thenReturn(1,1);
+        when(asker.askInt("Joueur 1 : 1 -> attaquer , 2-> parer ")).thenReturn(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+        when(asker.askInt("Joueur 2 : 1 -> attaquer , 2-> parer ")).thenReturn(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
         when(asker.askInt("1: rejouer , sinon arreter le programme")).thenReturn(3);
-        
+
         arene.buildFight(asker);
         assertTrue(asker.askPrint().contains("Le Vainqueur est : Joueur"));
     }
